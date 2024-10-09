a295b44bd2424e942231acca357409980d214d14
Marcelo Damasceno <marcelodamasceno@gmail.com>
2024-10-08 21:53:16 -0300

    feat: Adicionar - Listar todas as tarefas que estão completas e não completas - As tarefas completas devem estar em uma div chamada tarefas completas com o título tacheado (strike). - As tarefas não completas devem estar em uma div chamada tarefas pendentes. - Na listagem deve conter o nome do usuário, titulo da tarefa

--- src/componentes/ListaDeTarefas/index.jsx
+++ src/componentes/ListaDeTarefas/index.jsx
@@ -1,14 +1,49 @@
-import Tarefa from "../Tarefa"
-function ListaDeTarefas(){
-    let tarefas=['lavar loucas', 'aniversario do gilberto','pagar as contas']
+import { useState, useEffect } from "react";
+import Tarefa from "../Tarefa";
+import "./ListaDeTarefas.css";
 
-    function escreveTarefa(tarefa){
-        return <Tarefa titulo={tarefa}/>
-    }
+function ListaDeTarefas() {
+  const [tarefas, setTarefas] = useState([]);
+  const [usuarios, setUsuarios] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((data) => setTarefas(data));
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsuarios(data));
+  }, []);
+
+  function escreveTarefa(tarefa, completa) {
+    const usuario = usuarios.find((user) => user.id === tarefa.userId);
+    const userName = usuario ? usuario.name : "";
     return (
-        <div style={{backgroundColor:"gray", height:"100vh"}}>
-            {tarefas.map((tarefa)=>escreveTarefa(tarefa))}
-        </div>
-    )
+      <Tarefa
+        userName={userName}
+        titulo={tarefa.title}
+        key={tarefa.id}
+        completa={completa}
+      />
+    );
+  }
+
+  function filtrarTarefas(completas) {
+    return tarefas.filter((tarefa) => tarefa.completed === completas);
+  }
+
+  return (
+    <div className="light-gray container">
+      <div>
+        <h2>Tarefas Pendentes</h2>
+        {filtrarTarefas(false).map((tarefa) => escreveTarefa(tarefa))}
+      </div>
+      <div>
+        <h2>Tarefas Completas</h2>
+        {filtrarTarefas(true).map((tarefa) => escreveTarefa(tarefa, true))}
+      </div>
+    </div>
+  );
 }
-export default ListaDeTarefas
+
+export default ListaDeTarefas;
--- src/componentes/Tarefa/index.jsx
+++ src/componentes/Tarefa/index.jsx
@@ -1,6 +1,20 @@
-function Tarefa(props){
-    return (
-        <p style={{backgroundColor:"lightgray", textDecoration:"underline"}}>{props.titulo}</p>
-    )
+import PropTypes from "prop-types";
+import "./Tarefa.css";
+function Tarefa(props) {
+  return (
+    <div className={"tarefa-container"}>
+      <p>{props.userName}</p>
+      <p className={`tarefa ${props.completa ? "completa" : ""}`}>
+        {props.titulo}
+      </p>
+    </div>
+  );
 }
-export default Tarefa
+
+Tarefa.propTypes = {
+  completa: PropTypes.bool.isRequired,
+  titulo: PropTypes.string.isRequired,
+  userName: PropTypes.string.isRequired,
+};
+
+export default Tarefa;
